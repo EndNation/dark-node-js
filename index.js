770c37aab2b8e430b72cdfe4fcc4a31d201b014e
TheDarkSid3r <zakirullah03@gmail.com>
2018-06-10 13:02:18 +0800

    Added a few new functions...

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+var devs = [];
+
+function log(m)
+{
+    console.log("[LOG]:" + m);
+}
+
+function warn(m)
+{
+    console.warn("[WARN]:" + m);
+}
+
+function errors(m)
+{
+    console.error("[ERROR]:" + m);
+}
+
 exports.log = function(msg) {
-    console.log("[LOG]:" + msg);
+    log(msg);
 }
 
 exports.warn = function(warnMsg) {
-    console.warn("[WARN]:" + warnMsg);
+    warn(warnMsg);
+}
+
+exports.error = function(errMsg)
+{
+    errors(errMsg);
+}
+
+exports.addDev = function(devID)
+{
+    if (typeof devID === 'number')
+    {
+        errors("DevID must be a string!");
+    }
+    else if (typeof devID === 'string')
+    {
+        devs.push(devID);
+    }
 }
